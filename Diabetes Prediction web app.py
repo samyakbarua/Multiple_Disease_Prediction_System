9910161b7d5ffc18c751ae6c337856d1f2354f5c
samyakbarua <142702982+samyakbarua@users.noreply.github.com>
2025-05-09 20:47:34 +0530

    Update Diabetes Prediction web app.py

--- Diabetes Prediction web app.py
+++ Diabetes Prediction web app.py
@@ -81,9 +81,9 @@ if selected == 'Diabetes Prediction':
         diab_prediction = diabetes_model.predict([user_input])
 
         if diab_prediction[0] == 1:
-            diab_diagnosis = 'The person is diabetic'
-        else:
             diab_diagnosis = 'The person is not diabetic'
+        else:
+            diab_diagnosis = 'The person is diabetic'
 
     st.success(diab_diagnosis)
 
